0287f8e39994a3ddf6e0d5f8db49b8ca00d2ff85
mariaalicen <mariaalicen12@outlook.com>
2024-05-31 22:23:46 -0300

    adc funcoes a arvore generica

--- Arvore/arvore/generica.c
+++ Arvore/arvore/generica.c
@@ -5,7 +5,9 @@ struct no {
     int filhos[3];
 };
 
-void exibir(struct no exemplo1[8]) {
+struct no exemplo1[15];
+
+void exibir(struct no exemplo1[15]) {
     for (int i = 0; i < 8; i++) {
         printf("\n pai: %c \n", exemplo1[i].letra);
         for (int j = 0; j < 3; j++) {
@@ -14,15 +16,38 @@ void exibir(struct no exemplo1[8]) {
     }
 }
 
-void inserir(struct no exemplo1[8]) {
+void inserir(struct no exemplo1[15]) {
     exemplo1[0].letra = 'A';
     exemplo1[0].filhos[0] = 1;
     exemplo1[1].letra = 'B';
+    exemplo1[1].filhos[0] = 5;
     exemplo1[2].letra = 'C';
+    exemplo1[3].letra = 'D';
+    exemplo1[4].letra = 'E';
+    exemplo1[5].letra = 'F';
+    exemplo1[6].letra = 'G';
+    exemplo1[7].letra = 'H'; 
 }
 
-void buscaProfundidade(struct no exemplo1[8], int indice) {
-    if (indice >= 0 && indice < 8 && exemplo1[indice].letra != '\0') {
+void inserirInoE(struct no exemplo1[15]){
+    exemplo1[8].letra = 'I';
+    exemplo1[4].filhos[0] = 2; 
+}
+
+void inserirJnoG(struct no exemplo1[15]){
+    exemplo1[9].letra = 'J';
+    exemplo1[6].filhos[0] = 3; 
+}
+
+void remover(struct no exemplo1[15], int indice) {
+    exemplo1[indice].letra = '\0';
+    for (int j = 0; j < 3; j++) {
+        exemplo1[indice].filhos[j] = -1;
+    }
+}
+
+void buscaProfundidade(struct no exemplo1[15], int indice) {
+    if (indice >= 0 && indice < 15 && exemplo1[indice].letra != '\0') {
         printf("%c ", exemplo1[indice].letra);
 
         for (int i = 0; i < 3; i++) {
@@ -34,9 +59,7 @@ void buscaProfundidade(struct no exemplo1[8], int indice) {
 }
 
 int main() {
-    struct no exemplo1[8];
-
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < 15; i++) {
         exemplo1[i].letra = '\0'; // Nós vazios
         for (int j = 0; j < 3; j++) {
             exemplo1[i].filhos[j] = -1; // Filhos vazios
@@ -45,9 +68,12 @@ int main() {
 
     inserir(exemplo1);
     exibir(exemplo1);
-
-    printf("\n\n --------- Busca em Profundidade ---------------\n\n");
     buscaProfundidade(exemplo1, 0); // Começa a busca a partir do nó raiz
-
+    inserirInoE(exemplo1);
+    inserirJnoG(exemplo1);
+    exibir(exemplo1);
+    remover(exemplo1, 7); 
+    remover(exemplo1, 3); 
+    
     return 0;
 }
